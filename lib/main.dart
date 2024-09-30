665a072b85979d89a2d36ba9c9fc24cec1560667
Nielvin Amancio <112628938+Amanciolicious@users.noreply.github.com>
2024-09-30 15:12:12 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:farmer_app/pages/home_page.dart';
+
+void main() {
+  runApp(const MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Agri-Connect',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+        
+      ),
+      home: const HomePage(),
+    );
+  }
+}
