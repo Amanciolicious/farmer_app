da3247e03767e4a7be497c952ec3c7c11f962097
Nielvin Amancio <112628938+Amanciolicious@users.noreply.github.com>
2024-10-15 14:49:03 +0800

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:badges/badges.dart' as badges;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,15 +13,35 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: const Drawer(
-        child: Text("Data"),
-        backgroundColor: Colors.amber,
-      ),
+      drawer: const Drawer(),
       appBar: AppBar(
-        title: const Column(
+        centerTitle: false,
+        title: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [Text("Hi Nielvin."), Text("Connect with Farmers.")],
+          children: [
+            Text("Hi Amancio 🫀", style: Theme.of(context).textTheme.titleMedium,),
+            Text("Enjoy our services.", style: Theme.of(context).textTheme.bodySmall),
+          ],
         ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton.filledTonal(
+              onPressed: () {}, 
+              icon: badges.Badge(
+                badgeContent: const Text(
+                  '3',
+                   style: TextStyle(color: Colors.white, fontSize: 12,),
+                   ),
+                badgeStyle: const badges.BadgeStyle(
+                  badgeColor: Colors.green,
+                ),
+                position: badges.BadgePosition.topEnd(top: -15, end: -12),
+                child: const Icon(IconlyBroken.notification),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
