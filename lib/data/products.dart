f5772e042f1885eaac8e0640e9f10106f1a92e29
Nielvin Amancio <112628938+Amanciolicious@users.noreply.github.com>
2024-11-05 14:23:22 +0800

    Add files via upload

---
+++ lib/data/products.dart
@@ -0,0 +1,52 @@
+import '../models/product.dart';
+
+List<Product> products = [
+  const Product(
+    name: "Cabbage", 
+    description: "EgoCentric", 
+    image: 'assets/cabbage.jpg', 
+    price: 502.60, 
+    unit: 'day', 
+    rating: 4.35
+  ),
+  const Product(
+    name: "Carrot", 
+    description: "EgoCentric", 
+    image: 'assets/carrot.jpg', 
+    price: 502.60, 
+    unit: 'day', 
+    rating: 4.35
+  ),
+  const Product(
+    name: "Onion", 
+    description: "EgoCentric", 
+    image: 'assets/onion.jpg', 
+    price: 502.60, 
+    unit: 'day', 
+    rating: 4.35
+  ),
+  const Product(
+    name: "Pechay", 
+    description: "EgoCentric", 
+    image: 'assets/pechay.jpg', 
+    price: 502.60, 
+    unit: 'day', 
+    rating: 4.35
+  ),
+  const Product(
+    name: "Pumpkin", 
+    description: "EgoCentric", 
+    image: 'assets/pumpikin.jpg', 
+    price: 502.60, 
+    unit: 'day', 
+    rating: 4.35
+  ),
+  const Product(
+    name: "Tomato", 
+    description: "EgoCentric", 
+    image: 'assets/tomato.jpg', 
+    price: 502.60, 
+    unit: 'day', 
+    rating: 4.35
+  ),
+];
