562da29b7d046b369d0f4b5c28540a959e2c2bf9
Nielvin Amancio <112628938+Amanciolicious@users.noreply.github.com>
2024-11-07 13:09:22 +0800

    Update products.dart

--- lib/data/products.dart
+++ lib/data/products.dart
@@ -5,48 +5,48 @@ List<Product> products = [
     name: "Cabbage", 
     description: "EgoCentric", 
     image: 'assets/cabbage.jpg', 
-    price: 502.60, 
+    price: 50.60, 
     unit: 'day', 
-    rating: 4.35
+    rating: 5.35
   ),
   const Product(
     name: "Carrot", 
     description: "EgoCentric", 
     image: 'assets/carrot.jpg', 
-    price: 502.60, 
+    price: 42.60, 
     unit: 'day', 
-    rating: 4.35
+    rating: 7.35
   ),
   const Product(
     name: "Onion", 
     description: "EgoCentric", 
     image: 'assets/onion.jpg', 
-    price: 502.60, 
+    price: 20.60, 
     unit: 'day', 
-    rating: 4.35
+    rating: 5.50
   ),
   const Product(
     name: "Pechay", 
     description: "EgoCentric", 
     image: 'assets/pechay.jpg', 
-    price: 502.60, 
+    price: 35.60, 
     unit: 'day', 
-    rating: 4.35
+    rating: 8.01
   ),
   const Product(
     name: "Pumpkin", 
     description: "EgoCentric", 
     image: 'assets/pumpikin.jpg', 
-    price: 502.60, 
+    price: 51.60, 
     unit: 'day', 
-    rating: 4.35
+    rating: 6.41
   ),
   const Product(
     name: "Tomato", 
     description: "EgoCentric", 
     image: 'assets/tomato.jpg', 
-    price: 502.60, 
+    price: 22.60, 
     unit: 'day', 
-    rating: 4.35
+    rating: 9.32
   ),
 ];
